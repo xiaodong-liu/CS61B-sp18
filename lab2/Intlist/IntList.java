c1af920f9c0cd6c84859231bc582f78ec5c84280
Xiaodong <573523059@qq.com>
2020-01-18 21:45:23 +0800

    lab2 completed again

--- lab2/Intlist/IntList.java
+++ lab2/Intlist/IntList.java
@@ -82,6 +82,8 @@ public class IntList {
 
     public static IntList dcatenate(IntList A, IntList B) {
         //TODO:  fill in method
+        if (A == null)
+            return B;
         IntList temp = A;
         while (temp.rest != null)
             temp = temp.rest;
