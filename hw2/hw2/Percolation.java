51d3bc4d7e272177fd6fa140338a5683912cccdf
Xiaodong <573523059@qq.com>
2020-02-10 14:12:59 +0800

    HW2 completed

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -8,6 +8,7 @@ public class Percolation {
     private int N;
     private int count;
     private WeightedQuickUnionUF DS;
+    private WeightedQuickUnionUF DSNoBottom;
     private int up;
     private int bottom;
 
@@ -18,15 +19,11 @@ public class Percolation {
             throw new IllegalArgumentException("Error argument");
         }
         DS = new WeightedQuickUnionUF(N * N + 2);
+        DSNoBottom = new WeightedQuickUnionUF(N * N + 2);
         up = N * N;
         bottom = N * N + 1;
         this.N = N;
         site = new boolean[N][N];
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                site[i][j] = false;
-            }
-        }
         count = 0;
 
     }
@@ -41,32 +38,32 @@ public class Percolation {
 
             if (row == 0) {
                 DS.union(col, up);
+                DSNoBottom.union(col, up);
             }
-            connectBottom();
+            if (row == N - 1) {
+                DS.union(row * N + col, bottom);
+            }
         }
     }
 
-    private void connectBottom() {
-        for (int i = 0; i < N; i++) {
-            if (DS.connected(N * (N - 1) + i, up)) {
-                DS.union(N * (N - 1) + i, bottom);
-            }
-        }
-    }
 
     private void connectNeighbours(int row, int col) {
         int currentIndex = N * row + col;
         if ((row - 1 >= 0) && isOpen(row - 1, col)) {
             DS.union(currentIndex, N * (row - 1) + col);
+            DSNoBottom.union(currentIndex, N * (row - 1) + col);
         }
         if ((row + 1 < N) && isOpen(row + 1, col)) {
             DS.union(currentIndex, N * (row + 1) + col);
+            DSNoBottom.union(currentIndex, N * (row + 1) + col);
         }
         if ((col - 1 >= 0) && isOpen(row, col - 1)) {
             DS.union(currentIndex, N * row + col - 1);
+            DSNoBottom.union(currentIndex, N * row + col - 1);
         }
         if ((col + 1 < N) && isOpen(row, col + 1)) {
             DS.union(currentIndex, N * row + col + 1);
+            DSNoBottom.union(currentIndex, N * row + col + 1);
         }
     }
 
@@ -81,7 +78,7 @@ public class Percolation {
     public boolean isFull(int row, int col) {
         isLegal(row, col);
 
-        return DS.connected(N * row + col, up);
+        return DSNoBottom.connected(N * row + col, up);
     }
 
     //number of open site
